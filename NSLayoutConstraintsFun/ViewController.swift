9df8e1c11fde9c2717dbd22d667476eab4228790
Yung Dai <yungchidai@gmail.com>
2015-06-18 10:31:22 -0400

    Initial Commit

---
+++ NSLayoutConstraintsFun/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  NSLayoutConstraintsFun
+//
+//  Created by Yung Dai on 2015-06-18.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var leftView: UIView!
+    @IBOutlet var rightView: UIView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
