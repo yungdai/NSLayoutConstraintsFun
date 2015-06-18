100f6cec713d8a1e5fca4cc64ad4370da6b41f79
Yung Dai <yungchidai@gmail.com>
2015-06-18 17:51:23 -0400

    synch

--- NSLayoutConstraintsFun/ViewController.swift
+++ NSLayoutConstraintsFun/ViewController.swift
@@ -13,17 +13,50 @@ class ViewController: UIViewController {
     @IBOutlet var leftView: UIView!
     @IBOutlet var rightView: UIView!
     
+    // set up the iPad constraints
+    let leftiPadWidth:CGFloat = 126
+    let rightiPadWidth:CGFloat = 235
+    
+    // set up the iPhone constraints
+    let leftiPhoneWidth:CGFloat = 40
+    let rightiPhoneWidth:CGFloat = 100
     
     
+    // outlets for the left and right views
+    @IBOutlet var leftViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet var rightViewWidthConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        applyIdiumSpecificConstraints()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func applyIdiumSpecificConstraints() {
+        
+        // check the user device type and then assign the constraints per device
+        
+        if UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Pad {
+            
+            // in case of iPad device
+            leftViewWidthConstraint.constant = leftiPadWidth
+            rightViewWidthConstraint.constant = rightiPadWidth
+          
+            
+        } else if UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Phone {
+            
+            // in case of iPhone device
+            leftViewWidthConstraint.constant = leftiPhoneWidth
+            rightViewWidthConstraint.constant = rightiPhoneWidth
+            
+        } else {
+            
+            // in case I just don't know
+            leftViewWidthConstraint.constant = 0.0
+            rightViewWidthConstraint.constant = 0.0
+        }
+        
     }
 
 
